ac31c25892c47069fc914eef46fdff7c13ec2a0f
SofiaHritcu <61913547+SofiaHritcu@users.noreply.github.com>
2021-08-31 13:21:23 +0300

    #google - update: added missing lines from Startup

--- Startup.cs
+++ Startup.cs
@@ -40,13 +40,8 @@ namespace AspNetSandBox
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiSandbox v1"));
             }
 
             app.UseHttpsRedirection();
