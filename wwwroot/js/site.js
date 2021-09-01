80d1e8ceb1d298c711aee3d08979e59a226e7884
SofiaHritcu <61913547+SofiaHritcu@users.noreply.github.com>
2021-09-01 11:40:20 +0300

    #weather-forecast-open - add: default files with options
    paragraph weather added

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -22,7 +22,7 @@ const getWeather = async () => {
 
 function generateHTMLDailyWeatherForecasts(weather, index) {
     document.getElementById("timeFromNow" + index).textContent = weather.dailyForecasts[index].timeFromNow;
-    document.getElementById("currentWeatherIcon" + index).setAttribute('src', 'http://openweathermap.org/img/wn/' + weather.dailyForecasts[index].dailyIconCode + '@2x.png');
+    document.getElementById("currentWeatherIcon" + index).setAttribute('src', 'https://openweathermap.org/img/wn/' + weather.dailyForecasts[index].dailyIconCode + '@2x.png');
     document.getElementById("currentDescription" + index).textContent = weather.dailyForecasts[index].dailyDescription;
     document.getElementById("speedWind" + index).textContent = weather.dailyForecasts[index].windSpeed + ' m/s';
     document.getElementById("currentTemperature" + index).textContent = weather.dailyForecasts[index].dailyTemperature + '°C';
@@ -30,13 +30,13 @@ function generateHTMLDailyWeatherForecasts(weather, index) {
 
 const main = async () => {
     let weather = await getWeather();
-    document.getElementById('currentWeatherIcon').setAttribute('src', 'http://openweathermap.org/img/wn/' + weather.currentIconCode + '@2x.png')
+    document.getElementById('currentWeatherIcon').setAttribute('src', 'https://openweathermap.org/img/wn/' + weather.currentIconCode + '@2x.png')
     document.getElementById('currentDescription').textContent = weather.currentDescription;
     document.getElementById('currentTemperature').textContent = weather.currentTemperature + '°C';
 
     // hours from now
     document.getElementById("timeFromNowHours").textContent = weather.dailyForecasts[0].timeFromNow;
-    document.getElementById("currentWeatherIconHours").setAttribute('src', 'http://openweathermap.org/img/wn/' + weather.dailyForecasts[0].dailyIconCode + '@2x.png');
+    document.getElementById("currentWeatherIconHours").setAttribute('src', 'https://openweathermap.org/img/wn/' + weather.dailyForecasts[0].dailyIconCode + '@2x.png');
     document.getElementById("currentDescriptionHours").textContent = weather.dailyForecasts[0].dailyDescription;
     document.getElementById("speedWindHours").textContent = weather.dailyForecasts[0].windSpeed + ' m/s';
     document.getElementById("currentTemperatureHours").textContent = weather.dailyForecasts[0].dailyTemperature + '°C';
