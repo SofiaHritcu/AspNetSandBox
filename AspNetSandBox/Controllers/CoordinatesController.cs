4cfe8cc5d2589faa26aa34a6690f8a1615d89828
SofiaHritcu <61913547+SofiaHritcu@users.noreply.github.com>
2021-09-02 23:47:37 +0300

    #coordinates - add: making request to api
    cityName & apiKey private fields to controller

--- AspNetSandBox/Controllers/CoordinatesController.cs
+++ AspNetSandBox/Controllers/CoordinatesController.cs
@@ -13,8 +13,22 @@ namespace AspNetSandBox.Controllers
     [Route("[controller]")]
     public class CoordinatesController : ControllerBase
     {
-        public IEnumerable<Coordinates> Get()
+        private string apiKey;
+        private string cityName;
+        public CoordinatesController(string apiKey, string cityName)
         {
+            this.apiKey = apiKey;
+            this.cityName = cityName;
+        }
+
+        [HttpGet]
+        public IEnumerable<Coordinates> Get()
+        {   // Berlin 56bb96d9fedf1b3044e60b0760f4278d .AddParameter("q", "Berlin")
+            var client = new RestClient("https://api.openweathermap.org/data/2.5//weather");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET).AddQueryParameter("q", "Berlin").AddQueryParameter("appid", "56bb96d9fedf1b3044e60b0760f4278d");
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
             return new List<Coordinates>();
 
         }
