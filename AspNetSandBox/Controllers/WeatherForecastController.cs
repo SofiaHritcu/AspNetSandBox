cc4ac20c8b977003e6638c082d9892651b3dc50c
SofiaHritcu <61913547+SofiaHritcu@users.noreply.github.com>
2021-09-02 10:36:05 +0300

    #weather-forecast - add: get TemperatureC from json response

--- AspNetSandBox/Controllers/WeatherForecastController.cs
+++ AspNetSandBox/Controllers/WeatherForecastController.cs
@@ -27,7 +27,7 @@ namespace AspNetSandBox.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            var client = new RestClient("https://api.openweathermap.org/data/2.5/onecall?lat=45.657974&lon=25.601198&exclude=hourly,minutely&appid=56bb96d9fedf1b3044e60b0760f4278d");
+            var client = new RestClient("https://api.openweathermap.org/data/2.5/onecall?lat=45.657974&lon=25.601198&exclude=hourly,minutely&units=metric&appid=56bb96d9fedf1b3044e60b0760f4278d");
             client.Timeout = -1;
             var request = new RestRequest(Method.GET);
             IRestResponse response = client.Execute(request);
@@ -57,9 +57,9 @@ namespace AspNetSandBox.Controllers
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
+                TemperatureC = (int)json["daily"][0]["temp"].Value<double>("day"),
                 Summary = json["daily"][0]["weather"][0].Value<string>("main")
-            }) 
+            })
             .ToArray();
         }
     }
