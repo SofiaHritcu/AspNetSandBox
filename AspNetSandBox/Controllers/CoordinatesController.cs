223307221ce2b07da4cb0baa2f190bcdbd1ebb21
SofiaHritcu <61913547+SofiaHritcu@users.noreply.github.com>
2021-09-02 23:15:35 +0300

    #coordinates - add: controller class for coordinates and coordinates entity with lat and long

---
+++ AspNetSandBox/Controllers/CoordinatesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace AspNetSandBox.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CoordinatesController : ControllerBase
+    {
+        public IEnumerable<Coordinates> Get()
+        {
+            return new List<Coordinates>();
+
+        }
+
+        public IEnumerable<Coordinates> ConvertResponseToWeatherForecast(string content)
+        {
+            return new List<Coordinates>();
+        }
+
+        
+    }
+}
---
+++ AspNetSandBox/Coordinates.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetSandBox
+{
+    public class Coordinates
+    {
+        public double  Lat { get; set; }
+
+        public double Long { get; set; }
+    }
+}
