749003f5b037d922f0ba1f7a45b248101f80e2c8
SofiaHritcu <61913547+SofiaHritcu@users.noreply.github.com>
2021-09-03 10:40:09 +0300

    #books - add: book entity and controller

---
+++ AspNetSandBox/BooksController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspNetSandBox
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private Book[] books;
+
+        public BooksController()
+        {
+            books = new Book[2];
+            books[0] = new Book();
+            books[0].Id = 1;
+            books[0].Title = "To Kill A Mocking Bird";
+            books[0].Author = "Harper Lee";
+            books[0].Language = "english";
+
+            books[1] = new Book();
+            books[1].Id = 2;
+            books[1].Title = "Crime&Punishment";
+            books[1].Author = "Feodor Dostoievski";
+            books[1].Language = "english";
+        }
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return books;
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
