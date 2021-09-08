d9717ae84c95ef319a067a0c68a0e9bbdc142a7e
SofiaHritcu <61913547+SofiaHritcu@users.noreply.github.com>
2021-09-08 19:46:07 +0300

    #AspNetSandbox -update: cleaning code from books controller

--- AspNetSandBox/Controllers/BooksController.cs
+++ AspNetSandBox/Controllers/BooksController.cs
@@ -1,29 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace AspNetSandBox.Controllers
 {
+    /// <summary>
+    /// BooksController .
+    /// Exposes api CRUD operations for books.
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
-
-    ///  <summary>
-    ///  BooksController
-    /// </summary>
     public class BooksController : ControllerBase
     {
         private IBooksService booksService;
-        
+
+        /// <summary>Initializes a new instance of the <see cref="BooksController" /> class.</summary>
+        /// <param name="booksService">The books service.</param>
         public BooksController(IBooksService booksService)
         {
             this.booksService = booksService;
         }
 
         // GET: api/<ValuesController>
+
+        /// <summary>Gets all the instances of books.</summary>
+        /// <returns>Enumerable of book objects.<br /></returns>
         [HttpGet]
         public IEnumerable<Book> Get()
         {
@@ -31,26 +33,38 @@ namespace AspNetSandBox.Controllers
         }
 
         // GET api/<ValuesController>/5
+
+        /// <summary>Gets the specified book by id.</summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns>book object.</returns>
         [HttpGet("{id}")]
         public ActionResult Get(int id)
         {
             try
             {
                 return Ok(booksService.Get(id));
-            } catch (Exception e)
+            }
+            catch (Exception)
             {
                 return NotFound();
             }
         }
 
         // POST api/<ValuesController>
+
+        /// <summary>Posts the specified book.</summary>
+        /// <param name="value">The value.</param>
         [HttpPost]
         public void Post([FromBody] Book value)
         {
             booksService.Add(value);
         }
 
-        // PUT api/<ValuesController>/5
+        // PUT api/<ValuesController>/
+
+        /// <summary>Updates the book at the specified id with the fields of value.</summary>
+        /// <param name="id">The identifier.</param>
+        /// <param name="value">The value.</param>
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] Book value)
         {
@@ -58,6 +72,9 @@ namespace AspNetSandBox.Controllers
         }
 
         // DELETE api/<ValuesController>/5
+
+        /// <summary>Deletes the book found at the specified identifier.</summary>
+        /// <param name="id">The identifier.</param>
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
