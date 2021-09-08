fee125d94f7e1f809c903dc0a733671c0d6bce9d
SofiaHritcu <61913547+SofiaHritcu@users.noreply.github.com>
2021-09-08 09:55:59 +0300

    #AspNetSandBox - update: return http responses for getting book by id

--- AspNetSandBox/BooksController.cs
+++ AspNetSandBox/BooksController.cs
@@ -28,9 +28,15 @@ namespace AspNetSandBox
 
         // GET api/<ValuesController>/5
         [HttpGet("{id}")]
-        public Book Get(int id)
+        public ActionResult Get(int id)
         {
-            return booksService.Get(id);
+            try
+            {
+                return Ok(booksService.Get(id));
+            } catch (Exception e)
+            {
+                return NotFound();
+            }
         }
 
         // POST api/<ValuesController>
