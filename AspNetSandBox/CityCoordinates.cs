a17254078fd70d1f8aa929fd1ba553e36c9f2b8b
SofiaHritcu <61913547+SofiaHritcu@users.noreply.github.com>
2021-09-08 20:21:21 +0300

    #AspNetSandBox - update: clean code for City Coordinates class

--- AspNetSandBox/CityCoordinates.cs
+++ AspNetSandBox/CityCoordinates.cs
@@ -5,10 +5,17 @@ using System.Threading.Tasks;
 
 namespace AspNetSandBox
 {
+    /// <summary>
+    ///   <para>City Coordinates class.<br /></para>
+    /// </summary>
     public class CityCoordinates
     {
-        public double  Lat { get; set; }
+        /// <summary>Gets or sets the lat.</summary>
+        /// <value>The lat.</value>
+        public double Lat { get; set; }
 
+        /// <summary>Gets or sets the long.</summary>
+        /// <value>The long.</value>
         public double Long { get; set; }
     }
 }
