833f02da7aefcac8d8fa56941194e4b83e76843b
SofiaHritcu <61913547+SofiaHritcu@users.noreply.github.com>
2021-09-08 20:04:18 +0300

    #AspNetSandBox - update: clean code in city coordinates controller

--- AspNetSandBox/Controllers/CityCoordinatesController.cs
+++ AspNetSandBox/Controllers/CityCoordinatesController.cs
@@ -5,22 +5,31 @@ using RestSharp;
 
 namespace AspNetSandBox.Controllers
 {
+    /// <summary>CityCoordinatesController gets city coordinates from third party api.</summary>
     [ApiController]
     [Route("[controller]")]
     public class CityCoordinatesController : ControllerBase
     {
         private string apiKey;
         private string cityName;
-        
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CityCoordinatesController"/> class.
+        /// </summary>
+        /// <param name="cityName">Name of the city.</param>
         public CityCoordinatesController(string cityName)
         {
             this.apiKey = "56bb96d9fedf1b3044e60b0760f4278d";
             this.cityName = cityName;
         }
 
+        /// <summary>Gets coordinates for city.</summary>
+        /// <returns>
+        ///   <para>city coordinates object.<br /></para>
+        /// </returns>
         [HttpGet]
         public CityCoordinates Get()
-        {   
+        {
             var client = new RestClient("https://api.openweathermap.org/data/2.5//weather");
             client.Timeout = -1;
             var request = new RestRequest(Method.GET).AddQueryParameter("q", cityName).AddQueryParameter("appid", apiKey);
@@ -30,6 +39,9 @@ namespace AspNetSandBox.Controllers
             return ConvertResponseToCityCoordinates(response.Content);
         }
 
+        /// <summary>Converts the response from api to city coordinates.</summary>
+        /// <param name="content">The content.</param>
+        /// <returns>city coordinates object.<br /></returns>
         [NonAction]
         public CityCoordinates ConvertResponseToCityCoordinates(string content)
         {
@@ -40,9 +52,8 @@ namespace AspNetSandBox.Controllers
             return new CityCoordinates
             {
                 Lat = cityCoordinates.Value<double>("lat"),
-                Long = cityCoordinates.Value<double>("lon")
+                Long = cityCoordinates.Value<double>("lon"),
             };
         }
-        
     }
 }
