84e6977f1e988395ef0014a275b252538850ee73
SofiaHritcu <61913547+SofiaHritcu@users.noreply.github.com>
2021-09-10 00:22:35 +0300

    #AspNetSandBox - add: delete book implementation

---
+++ AspNetSandBox/wwwroot/js/delete-book.js
@@ -0,0 +1,22 @@
+﻿async function deleteBook(buttonDelete) {
+    // get id of book to be deleted
+    const bookId = buttonDelete.parentNode.parentNode.getAttribute('data-id');
+    console.log(bookId)
+
+    // call backend for deleting book
+    await deleteBookCall(bookId);
+}
+
+// call to backend api to delete book
+async function deleteBookCall(bookId) {
+    fetch(`/api/books/${bookId}`,{
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+    .then(async function () {
+        await fetchBooks();
+        alert('You have deleted the book!', 'success');
+    });
+}
