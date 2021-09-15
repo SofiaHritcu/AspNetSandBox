bf151126b57d20a42863504927ab642aa5bc808c
SofiaHritcu <61913547+SofiaHritcu@users.noreply.github.com>
2021-09-15 12:37:05 +0300

    #AspNetSandBox - add: book added in message from server

--- AspNetSandBox/wwwroot/js/books.js
+++ AspNetSandBox/wwwroot/js/books.js
@@ -10,4 +10,16 @@ connection.start().then(function () {
 
 connection.on("BookCreated", function (book) {
     console.log(`Book Created: ${JSON.stringify(book)}`);
+    $("#books").append(`<tr data-id="${book.id}">
+                            <td>${book.title}</td>
+                            <td>${book.author}</td>
+                            <td>${book.language}</td>
+                            <td class="text-center">
+                                <a type="button" data-toggle="modal" data-target="#updateBookModal" class="btn btn-outline-dark mx-2" onclick="updateBook(this)">
+                                    <i class="bi bi-pencil-square"></i> Edit</a><button type="button" class="btn btn-outline-dark mx-2" onclick="deleteBook(this)">
+                                    <i class="bi bi-trash"></i> Delete</button><button type="button" data-toggle="modal" data-target="#getBookModal" class="btn btn-outline-dark mx-2" onclick="getBook(this)">
+                                    <i class="bi bi-search"></i> Details</button>
+                            </td>
+                        </tr>`);
+    
 });
