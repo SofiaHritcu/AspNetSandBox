ba440a7fd7e33f4eb4dd15fb5e02a46279034038
SofiaHritcu <61913547+SofiaHritcu@users.noreply.github.com>
2021-09-17 08:05:06 +0300

    #AspNetSandBox - add: imports in tests for CityCoordinates and WeatherForecast

--- AspNetSandBox.Tests/CityCoordinatesControllerTest.cs
+++ AspNetSandBox.Tests/CityCoordinatesControllerTest.cs
@@ -1,4 +1,5 @@
 ﻿using AspNetSandBox.Controllers;
+using AspNetSandBox.Models;
 using System;
 using System.Collections.Generic;
 using System.IO;
--- AspNetSandBox.Tests/WeatherForecastControllerTest.cs
+++ AspNetSandBox.Tests/WeatherForecastControllerTest.cs
@@ -1,4 +1,5 @@
 using AspNetSandBox.Controllers;
+using AspNetSandBox.Models;
 using System;
 using System.Collections.Generic;
 using System.IO;
