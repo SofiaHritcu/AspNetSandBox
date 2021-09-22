c5da3de59364e328e0a440e85c303f43564876a9
SofiaHritcu <61913547+SofiaHritcu@users.noreply.github.com>
2021-09-22 11:55:24 +0300

    #AspNetSandBox - refactoring: solving warnings in Program

--- AspNetSandBox/Program.cs
+++ AspNetSandBox/Program.cs
@@ -10,8 +10,16 @@ using Microsoft.Extensions.Logging;
 
 namespace AspNetSandBox
 {
+    /// <summary>Options Class.</summary>
+#pragma warning disable SA1402 // File may only contain a single type
+#pragma warning disable SA1649 // File name should match first type name
     public class Options
+#pragma warning restore SA1649 // File name should match first type name
+#pragma warning restore SA1402 // File may only contain a single type
     {
+        /// <summary>Gets or sets a value indicating whether this <see cref="Options" /> is verbose.</summary>
+        /// <value>
+        ///   <c>true</c> if verbose; otherwise, <c>false</c>.</value>
         [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
         public bool Verbose { get; set; }
     }
