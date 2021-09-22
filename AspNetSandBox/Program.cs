dc5252c4171d3f3a138e75a23fcef54d60791c37
SofiaHritcu <61913547+SofiaHritcu@users.noreply.github.com>
2021-09-22 11:41:16 +0300

    #AspNetSandBox - add: verbose options in args

--- AspNetSandBox/Program.cs
+++ AspNetSandBox/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CommandLine;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -9,6 +10,12 @@ using Microsoft.Extensions.Logging;
 
 namespace AspNetSandBox
 {
+    public class Options
+    {
+        [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+        public bool Verbose { get; set; }
+    }
+
     /// <summary>Program Class.</summary>
     public class Program
     {
@@ -16,6 +23,21 @@ namespace AspNetSandBox
         /// <param name="args">The arguments.</param>
         public static int Main(string[] args)
         {
+            Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed<Options>(o =>
+                   {
+                       if (o.Verbose)
+                       {
+                           Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
+                           Console.WriteLine("Quick Start Example! App is in Verbose mode!");
+                       }
+                       else
+                       {
+                           Console.WriteLine($"Current Arguments: -v {o.Verbose}");
+                           Console.WriteLine("Quick Start Example!");
+                       }
+                   });
+
             if (args.Length > 0)
             {
                 Console.WriteLine($"There are {args.Length} args");
