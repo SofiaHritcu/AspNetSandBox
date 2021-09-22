ab90b504786173babcb72fe242a8d5fb5e141302
SofiaHritcu <61913547+SofiaHritcu@users.noreply.github.com>
2021-09-22 13:04:28 +0300

    #AspNetSandBox - test: file operations

--- AspNetSandBox.Tests/FileOperationWithBooksTests.cs
+++ AspNetSandBox.Tests/FileOperationWithBooksTests.cs
@@ -29,22 +29,43 @@ namespace AspNetSandBox.Tests
         [Fact]
         public void ShouldCreateFile()
         {
+#pragma warning disable SA1118 // Parameter should not span multiple lines
             File.WriteAllText("newSettings.json", @"{
-  ""ConnectionStrings"": {
-    ""DefaultConnection"": ""Database=d2ksfg273gcms;Host=ec2-44-194-225-27.compute-1.amazonaws.com;Port=5432;User Id=dosxxqgzjegxwt;Password=718652f7c895171b9a187f082390b5781443ea59062cf7673155f71b20276643; SSL Mode=Require; Trust Server Certificate=true"",
-    ""HerokuConnection"": ""Database=d2ksfg273gcms;Host=ec2-44-194-225-27.compute-1.amazonaws.com;Port=5432;User Id=dosxxqgzjegxwt;Password=718652f7c895171b9a187f082390b5781443ea59062cf7673155f71b20276643; SSL Mode=Require; Trust Server Certificate=true"",
-    ""PostgresConnection"": ""Server=127.0.0.1;Port=5432;Database=aspNetSandBox-sofia;User Id=postgres;Password=Euforie041229.;"",
-    ""SqlConnection"": ""Server=(localdb)\\mssqllocaldb;Database=aspnet-DBSandBox-C8ECF1DE-AB43-40A2-A0B9-1892667E188C;Trusted_Connection=True;MultipleActiveResultSets=true""
-  },
-  ""Logging"": {
-    ""LogLevel"": {
-      ""Default"": ""Information"",
-      ""Microsoft"": ""Warning"",
-      ""Microsoft.Hosting.Lifetime"": ""Information""
-    }
-  },
-  ""AllowedHosts"": ""*""
-}");
+                                      ""ConnectionStrings"": {
+                                        ""DefaultConnection"": ""Database=d2ksfg273gcms;Host=ec2-44-194-225-27.compute-1.amazonaws.com;Port=5432;User Id=dosxxqgzjegxwt;Password=718652f7c895171b9a187f082390b5781443ea59062cf7673155f71b20276643; SSL Mode=Require; Trust Server Certificate=true"",
+                                        ""HerokuConnection"": ""Database=d2ksfg273gcms;Host=ec2-44-194-225-27.compute-1.amazonaws.com;Port=5432;User Id=dosxxqgzjegxwt;Password=718652f7c895171b9a187f082390b5781443ea59062cf7673155f71b20276643; SSL Mode=Require; Trust Server Certificate=true"",
+                                        ""PostgresConnection"": ""Server=127.0.0.1;Port=5432;Database=aspNetSandBox-sofia;User Id=postgres;Password=Euforie041229.;"",
+                                        ""SqlConnection"": ""Server=(localdb)\\mssqllocaldb;Database=aspnet-DBSandBox-C8ECF1DE-AB43-40A2-A0B9-1892667E188C;Trusted_Connection=True;MultipleActiveResultSets=true""
+                                      },
+                                      ""Logging"": {
+                                        ""LogLevel"": {
+                                          ""Default"": ""Information"",
+                                          ""Microsoft"": ""Warning"",
+                                          ""Microsoft.Hosting.Lifetime"": ""Information""
+                                        }
+                                      },
+                                      ""AllowedHosts"": ""*""
+                                    }");
+#pragma warning restore SA1118 // Parameter should not span multiple lines
+        }
+
+        /// <summary>
+        /// Shoulds the read file.
+        /// </summary>
+        [Fact]
+        public void ShouldReadFile()
+        {
+            using (var fs = File.OpenRead("newSettings.json"))
+            {
+                byte[] b = new byte[1024];
+                UTF8Encoding temp = new UTF8Encoding(true);
+
+                while (fs.Read(b, 0, b.Length) > 0)
+                {
+                    var encodedString = temp.GetString(b);
+                    Console.WriteLine(encodedString);
+                }
+            }
         }
     }
 }
